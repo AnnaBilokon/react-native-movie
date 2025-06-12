4745b2e2d986dc1a814889a5b79e273921203546
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-06-12 10:14:46 +0200

    add Masked View

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,20 +1,46 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
+import MaskedView from "@react-native-masked-view/masked-view";
 
 const TrendingCard = ({
   movie: { movie_id, title, poster_url },
   index,
 }: TrendingCardProps) => {
   return (
-    <Link href={`/movies/${movie_id}` as any} asChild>
+    <Link href={`/movie/${movie_id}` as any} asChild>
       <TouchableOpacity className="w-32 relative pl-5">
         <Image
           source={{ uri: poster_url }}
           className="w-32 h-48 rounded-lg"
           resizeMode="cover"
         />
-        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full"></View>
+        <View className="absolute bottom-9 -left-3.5 px-2 py-1 rounded-full">
+          <MaskedView
+            maskElement={
+              <Text className="text-6xl font-bold text-white">{index + 1}</Text>
+            }
+          >
+            <Image
+              source={images.rankingGradient}
+              className="size-14"
+              resizeMode="cover"
+            />
+          </MaskedView>
+        </View>
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {title}
+        </Text>
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image
+            source={require("@/assets/icons/star.png")}
+            className="size-4"
+          />
+          <Text className="text-xs text-white font-bold uppercase">
+            {Math.round(8.5 / 2)} {/* Example rating, replace with actual */}
+          </Text>
+        </View>
       </TouchableOpacity>
     </Link>
   );
